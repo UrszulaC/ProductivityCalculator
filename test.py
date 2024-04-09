bc4181631ee2495e946e3f37bfd44389c5308b0a
Urszula <ulach@hotmail.co.uk>
2024-04-09 16:45:32 +0100

    Development branch added

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+import mysql.connector
+from config import *
+
+class TestProjectOperations(unittest.TestCase):
+
+    def setUp(self):
+        self.connection = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE
+        )
+        # Creating a test table for projects
+        self.create_test_table()
+
+    def tearDown(self):
+        # Clean up the test table after each test
+        self.drop_test_table()
+        self.connection.close()
