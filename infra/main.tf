be4caa07dd242b00974323b7beb048d0b95fa1ed
Ubuntu <ubuntu@ip-172-31-22-101.eu-west-2.compute.internal>
2024-09-20 13:30:43 +0000

    inventory added
    Former-commit-id: 5a0ec8c318d5e4bd304ed80c539f538da54cf695

--- infra/main.tf
+++ infra/main.tf
@@ -7,11 +7,13 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
+
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-west-2a"  
+  vpc_id              = aws_vpc.main.id
+  cidr_block          = "10.0.1.0/24"
+  availability_zone   = "eu-west-2a"  
 }
+
 resource "aws_security_group" "allow_http" {
   vpc_id = aws_vpc.main.id
 
@@ -29,13 +31,15 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_instance" "web" {
-  ami           = "ami-01ec84b284795cbc7"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
+  ami                    = "ami-01ec84b284795cbc7"
+  instance_type         = "t2.micro"
+  subnet_id             = aws_subnet.main.id
+  key_name              = "Server1_key"  
 
   root_block_device {
-    encrypted = true
+    encrypted   = true
     volume_size = 20
   }
 
